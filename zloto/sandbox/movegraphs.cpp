b143d14fb00dac03e7f7863fbc009da10805bc57
asifaaag <asifiqbal.rs@gmail.com>
2012-09-27 21:25:01 +0200

    Updated movegraphs.cpp with a function displayExprArray(), to show the expression within an array

--- zloto/sandbox/movegraphs.cpp
+++ zloto/sandbox/movegraphs.cpp
@@ -6,9 +6,9 @@
 #include<dos.h>         //for delay() function
 #include<stdio.h>
 #include<string.h>
- 
-#define XMID (getmaxx()/2);
-#define YMID (getmaxy()/2);
+
+#define XMID (getmaxx()/2)
+#define YMID (getmaxy()/2)
 #define SRC 1
 #define DST 0
 
@@ -40,6 +40,7 @@ class Expression{
 		char* getExpr(char* type);
 		void displayExpr(int x, int y, char* expr);
 		void initArray( char* type, int arrtype, int len);
+		void displayExprArray(char* expr);
 		void boundary();
 		void movDataE2E();
 		void movDataE2S();
@@ -55,36 +56,54 @@ void Expression :: boundary(){
 void Expression :: initArray(char* type, int arrtype, int len){
 	int x1, y1, x2, y2;
 	//len = strlen(expr);
-	x1 = XMID+20;
+	x1 = XMID;
 	x2 = x1;
-	y2 = arrtype? YMID-150: YMID+150;
-	y1 = y2-20;//upper edge for the source array
+
 	settextstyle( 0, HORIZ_DIR, 1);
 	setcolor(WHITE);
+	y2 = arrtype? YMID-150: YMID+150; //set y-axis according to source or dest type
+	y1 = y2-30;//upper edge for the source array
+
+	//draw internal lines for the array
 	for(int i=0; i <= len; i++){
 		line( x2, y1, x2, y2);
-		x2 = x2+20;
+		x2 = x2+30;
 	}
-	x2 = x2 - 20;
+	x2 = x2 - 30;
+	
+	//draw the two external lines for the array
 	line( x1, y1, x2, y1);
 	line( x1, y2, x2, y2);
 
 }
 
+void Expression :: displayExprArray(char* expr){
+	int x, y;
+	x = XMID+10;
+	y = YMID-162;
+	char* dump;
+	int len = strlen( expr);
+	settextstyle( 0, HORIZ_DIR, 1);
+	setcolor(WHITE);
+	for(int i=0; i<len; i++){
+		sprintf(dump,"%c",expr[i]);
+		outtextxy(x, y, dump);
+		x = x+20;
+	}
+}
+
 char* Expression :: getExpr(char* type){
 	char* expr;
-	int x = XMID+20;
+	int x = XMID;
 	int y = 20;
-	//for(int i=0;i<10;i++){
+	
 	settextstyle( 0, HORIZ_DIR, 1);
 	setcolor(WHITE);
 	sprintf( expr, "Enter the %s expression:", type);
-	//y=y+32;
 	outtextxy(x, y, expr);
-	//}
 	settextstyle( COMPLEX_FONT, HORIZ_DIR, 1);
 	setcolor(WHITE);
-	gotoxy( 41, 3);
+	gotoxy( 38, 3);
 	cin.getline( expr, 20);
 	return expr;
 }
@@ -112,8 +131,8 @@ void MoveGraph :: movDataY2Y( int X, int Ysrc, int Ydest){
 		setfillstyle(0, getmaxcolor());
 		circle(X+4,Ysrc+3,10);
 		///itoa(arr[count],string,10);
-		outtext("/");
-		delay(4);
+		outtext("(");
+		delay(20);
 		setfillstyle(0, getmaxcolor());
 		//setfillstyle( 1, BLUE);
 		bar(X-6 ,gety()-8 ,X+14 ,gety()+14);
@@ -147,20 +166,22 @@ void main(){
 	Expression eg;
 	char* infix;
 	cg.initGraph();
-	eg.boundary();
+	//eg.boundary();
 	setbkcolor(BLUE);
 	infix = eg.getExpr("Infix");
 	outtextxy( 10, 40, infix);
 	int len = strlen(infix);
 	eg.initArray( "Infix", SRC, len);
+	eg.displayExprArray(infix);
 	eg.initArray( "Postfix", DST, len);
-	
-/*	for(int i=0; i<=10; i++){
-	mg.movDataX2X(10,300,40);
-	mg.movDataY2Y(300,40,380);
-	mg.movDataX2X(300,10,380);
-	mg.movDataY2Y(10,380,40);
-	}
-	cg.closeGraph();*/
+	int x=XMID+20;
+	int y=YMID-162;
+//	for(int i=0; i<=10; i++){
+//	mg.movDataX2X(10,300,40);
+	mg.movDataY2Y(x,y,380);
+//	mg.movDataX2X(300,10,380);
+ //	mg.movDataY2Y(10,380,40);
+//	}
+//	cg.closeGraph();*/
 	getch();
 }
